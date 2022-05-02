e4e84451326969bef2f51154a1027bbaf8bd141d
Carlos <carlos-alc5@hotmail.com>
2022-05-02 08:46:55 -0500

    Nuevo reporistorio Api gatos

---
+++ Site de gatitos com api.mk
@@ -0,0 +1,14 @@
+# Criando site de gatinhos 
+
+Esse projeto tem como objetivo apresentar uma serie de fotos de gatinhos, interagindo com o usuario ao clicar no botão "change cat" para mudar de foto e ve routros gatinhos.
+
+Foi utilizado:
+
+- JavaScript
+- HTML
+- CSS
+- API que direciona o banco de dados das fotos dos gatinhos. 
+
+Embaixo uma foto, apresentação do site.
+
+<img src = "img/gatinhos.png">
